10c3dd85ce81b346b7acf167014f2a59a1898316
unknown <sven.lecann@live.fr>
2020-01-27 16:06:10 +0100

    serie entity

---
+++ src/Form/SerieType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Serie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SerieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('image')
+            ->add('releaseDate')
+            ->add('note')
+            ->add('imdbID')
+            ->add('overview')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Serie::class,
+        ]);
+    }
+}
